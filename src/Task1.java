f03c29f8c6a83f32f181b2bbf6e05ca2afc7abd2
vnskliarova <vnskliarova@gmail.com>
2020-10-24 15:19:54 -0400

    Task 1

--- src/Task1.java
+++ src/Task1.java
@@ -1,4 +1,12 @@
 
 public class Task1 {
+	public static void main(String[] args) {
 
+		int a = 15;
+		int b = 23;
+		a = a + b;
+		b = a - b;
+		a = a - b;
+		System.out.println(a + " " + b);
+	}
 }
