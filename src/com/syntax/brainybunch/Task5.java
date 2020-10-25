61452a1672661f87484337c8e7d2d371ad5e73c9
vnskliarova <vnskliarova@gmail.com>
2020-10-25 13:15:29 -0400

    Minor mendings

--- src/com/syntax/brainybunch/Task5.java
+++ src/com/syntax/brainybunch/Task5.java
@@ -1,10 +1,12 @@
 package com.syntax.brainybunch;
 
+import java.util.Arrays;
+
 public class Task5 {
 	public static void main(String[] args) {
 		// second largest number in the array
 
-		int[] array = { 8, 4, 9, 2, 15, 20, 19, 3 };
+		int[] array = { 8, 4, 9, 2, 15, 20, 20, 3 };
 		int max = array[0];
 		int secMax = array[0];
 
@@ -21,19 +23,24 @@ public class Task5 {
 		}
 		System.out.println("Second largest number is " + secMax);
 
-	
-		int[] findSecondLargestNumber = {17, 58, 67, 123, 32, 15, 64, 332, -55, -785, 84, 66, -88, 127};
+		System.out.println("----- 2nd way -----");
+
+		int[] findSecondLargestNumber = { 17, 58, 67, 123, 32, 15, 64, 332, -55, -785, 84, 66, -88, 127 };
 		int largestNumber = findSecondLargestNumber[0];
 		int secondLargestNumber = 0;
 		for (int i = 1; i < findSecondLargestNumber.length; i++) {
-		    if (findSecondLargestNumber[i] > largestNumber) {
-		        secondLargestNumber = largestNumber;
-		        largestNumber = findSecondLargestNumber[i];
-		    }
+			if (findSecondLargestNumber[i] > largestNumber) {
+				secondLargestNumber = largestNumber;
+				largestNumber = findSecondLargestNumber[i];
+			}
 		}
 		System.out.println("Largest Number: " + largestNumber + "\nSecond Largest Number: " + secondLargestNumber);
-	
-	
-	
+
+		System.out.println("----- 3rd way -----");
+		int[] num = { 8, 4, 9, 2, 15, 20, 19, 3 };
+		int length = num.length;
+		Arrays.sort(num);
+		System.out.println("Second largest number is " + num[length - 2]);
+
 	}
 }
