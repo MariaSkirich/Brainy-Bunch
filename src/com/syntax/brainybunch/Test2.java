cd589f400ea0afc47ac968be89e09f5ba70bd073
vnskliarova <vnskliarova@gmail.com>
2020-10-27 15:45:39 -0400

    Useless Commit, Playing with Intellij

---
+++ src/com/syntax/brainybunch/Test2.java
@@ -0,0 +1,8 @@
+package com.syntax.brainybunch;
+
+public class Test {
+    public static void main (String[] args){
+        System.out.println("Hello collegues");
+        System.out.println("Just checking out how it works");
+    }
+}
